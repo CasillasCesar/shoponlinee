e720e37f28d3ef4c48436fea1a47eccba0eef29c
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-09-04 23:47:55 -0600

    feat(app.module.ts): configuracion de modulo de componentes de primeNG
    Se termina la configuración de la aplicación para la correcta integración de las librerías de PrimeNG
    
    Closes #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Importing PrimeNG module
+import { PrimengModule } from "./primeng.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [
     provideClientHydration()
