e7d4783175c07d65bef092e67ae290b82e1939f4
CasillasCesar <cesarcasillascespedes@gmail.com>
2024-09-05 00:34:10 -0600

    Se agregan los modulos de primeNG que se emplearan para el desarrollo de la aplicacion

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Modulos importados de PrimeNG
+import { AvatarModule } from "primeng/avatar";
+import { ButtonModule } from "primeng/button";
+import { MenubarModule } from "primeng/menubar";
+import { ToolbarModule } from "primeng/toolbar";
+import { TooltipModule } from "primeng/tooltip";
+import { MenuModule } from "primeng/menu";
+
 //Modulos PrimeNG
-const modPrime : any = [];
+const modulesPrimeNG : any = [
+  AvatarModule,
+  ButtonModule,
+  MenubarModule,
+  ToolbarModule,
+  TooltipModule,
+  MenuModule
+];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    modPrime
+    modulesPrimeNG
   ],
   exports: [
-    modPrime
+    modulesPrimeNG
   ]
 })
 export class PrimengModule { }
